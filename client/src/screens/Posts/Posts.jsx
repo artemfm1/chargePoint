af095834fc0361510dd7e6c45b853bebeedad5ba
Artem Furman <af@Artems-MBP.fios-router.home>
2021-06-27 16:04:46 -0400

    mvp testing

--- client/src/screens/Posts/Posts.jsx
+++ client/src/screens/Posts/Posts.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Post from '../Post/Post'
+import './Post.css'
 
 
 export default function Posts(props) {
   const { posts } = props;
 
   return (
-    <div>
+    <div className="post-container">
       
     
       {posts.map(post => (
